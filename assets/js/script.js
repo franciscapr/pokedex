585490d5441a561ea904080195a59f5cb2b62502
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-11-25 09:42:11 -0300

    consulta con ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
-    $("form").submit(function(event){
+    $("form").submit(function (event) {
 
         event.preventDefault()
 
         let valueInput = $("#pokemonInput").val();
 
-        alert(valueInput);
-    })
+        $.ajax({
+            // Concatenamos el texto del pokemon + el valor de la variable
+            url: "https://pokeapi.co/api/v2/pokemon/" + valueInput,
+            success: function (data) {
+                let nombre = data.name
+                let imagen = data.sprites.front_default
+                let peso = data.weight;
+
+                $("#pokeInfo").html(`
+                    <div class="text-center">
+                        <h3>${nombre}</h3>
+                        <img src="${imagen}">
+                        <h6>Peso: ${peso}</h6>
+                     </div>`)
+            },
+        });
+    });
 });
